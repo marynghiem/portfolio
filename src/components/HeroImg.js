3e4e17fffdfab6f6e7912dcd164d7e4a6ac07d6a
marynghiem <marynghiem46@gmail.com>
2022-07-19 15:39:05 -0700

    created hero image component and assets folder to store images

---
+++ src/components/HeroImg.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Hero = styled.div``;
+const Mask = styled.div``;
+export const HeroImg = () => {
+  return (
+    <Hero>
+      <Mask></Mask>
+    </Hero>
+  );
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { HeroImg } from "../components/HeroImg";
 import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg />
     </div>
   );
 };

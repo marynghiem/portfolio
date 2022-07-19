d41406adc62c1be9f1e0287ab6f1bbe7b5e4c5d0
marynghiem <marynghiem46@gmail.com>
2022-07-19 14:08:57 -0700

    imported google fonts and created styling for buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Header = styled.div``;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const NavMenu = styled.ul`
+  display: flex;
+`;
+const NavMenuListItem = styled.li`
+  padding: 0 1rem;
+  font-size: 1.2rem;
+  font-weight: 500;
+`;
 
 export const Navbar = () => {
   return (
@@ -10,6 +24,20 @@ export const Navbar = () => {
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
+      <NavMenu>
+        <NavMenuListItem>
+          <Link to="/">Home</Link>
+        </NavMenuListItem>
+        <NavMenuListItem>
+          <Link to="/project">Project</Link>
+        </NavMenuListItem>
+        <NavMenuListItem>
+          <Link to="/about">About</Link>
+        </NavMenuListItem>
+        <NavMenuListItem>
+          <Link to="/contact">Contact</Link>
+        </NavMenuListItem>
+      </NavMenu>
     </Header>
   );
 };

26bcd39b0dca0944930965ddef4fe24d8e677596
marynghiem <marynghiem46@gmail.com>
2022-07-20 14:43:13 -0700

    made the footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,6 +13,9 @@ const FooterContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 40px;
+  @media (max-width: 640px) {
+    grid-template-columns: 1fr;
+  }
 `;
 const Left = styled.div`
   height: 100%;

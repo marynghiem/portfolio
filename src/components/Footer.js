e0ae5f259a0e1ce1ef81a16774d19fa65302e2f8
marynghiem <marynghiem46@gmail.com>
2022-07-20 14:17:53 -0700

    completed right side of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa";
+import { FaFacebook, FaHome, FaInstagram, FaMailBulk, FaPhone, FaTwitter } from "react-icons/fa";
 import styled from "styled-components";
 
-const WholeFooter = styled.div``;
+const WholeFooter = styled.div`
+  width: 100%;
+`;
 const FooterContainer = styled.div``;
 const Left = styled.div``;
 const Right = styled.div``;
 const Location = styled.div``;
 const Phone = styled.div``;
 const Email = styled.div``;
+const Social = styled.div``;
 
 export const Footer = () => {
   return (
@@ -35,7 +38,21 @@ export const Footer = () => {
             </h4>
           </Email>
         </Left>
-        <Right></Right>
+        <Right>
+          <h4>About Me</h4>
+          <p>
+            I am currently a year into my coding journey which started with FreeCodeCamp and learning
+            HTML/CSS/Javascript and React.js. I then learned CS fundamentals by completing the Harvard CS 50 course. I
+            am now actively building web applications using React.js to create enjoyable websites for users. In my
+            newest project I am collaborating with multiple industry full stack engineers to build a Pictionary game
+            with a Pokemon theme!
+          </p>
+          <Social>
+            <FaFacebook size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+            <FaTwitter size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+            <FaInstagram size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+          </Social>
+        </Right>
       </FooterContainer>
     </WholeFooter>
   );

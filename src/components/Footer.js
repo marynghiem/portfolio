a17f1dd8cb50a3b39459328eb4dfcfee5570c81f
marynghiem <marynghiem46@gmail.com>
2022-07-20 13:59:15 -0700

    coded all the information for the left side of the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa";
 import styled from "styled-components";
 
 const WholeFooter = styled.div``;
@@ -6,12 +7,34 @@ const FooterContainer = styled.div``;
 const Left = styled.div``;
 const Right = styled.div``;
 const Location = styled.div``;
+const Phone = styled.div``;
+const Email = styled.div``;
 
 export const Footer = () => {
   return (
     <WholeFooter>
       <FooterContainer>
-        <Left></Left>
+        <Left>
+          <Location>
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>San Jose.</p>
+              <p>California, United States.</p>
+            </div>
+          </Location>
+          <Phone>
+            <h4>
+              <FaPhone size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+              1-408-599-4331
+            </h4>
+          </Phone>
+          <Email>
+            <h4>
+              <FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+              marynghiem46@gmail.com
+            </h4>
+          </Email>
+        </Left>
         <Right></Right>
       </FooterContainer>
     </WholeFooter>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { HeroImg } from "../components/HeroImg";
 import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroImg />
+      <Footer />
     </div>
   );
 };

f8516fac90129144cf7a90392380c54352a014a7
marynghiem <marynghiem46@gmail.com>
2022-07-20 14:39:16 -0700

    styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,50 @@ import styled from "styled-components";
 
 const WholeFooter = styled.div`
   width: 100%;
+  padding: 6rem 0;
+  background-color: rgba(19, 19, 19, 0.8);
 `;
-const FooterContainer = styled.div``;
-const Left = styled.div``;
-const Right = styled.div``;
-const Location = styled.div``;
+const FooterContainer = styled.div`
+  max-width: 1140px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 40px;
+`;
+const Left = styled.div`
+  height: 100%;
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`;
+const Right = styled.div`
+  height: 100%;
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`;
+const Location = styled.div`
+  display: flex;
+  margin-bottom: 0.8rem;
+  p {
+    line-height: 30px;
+  }
+`;
 const Phone = styled.div``;
 const Email = styled.div``;
-const Social = styled.div``;
+const Social = styled.div`
+  margin-top: 1rem;
+`;
+
+const BoldWordsStyling = styled.h4`
+  font-size: 1.2rem;
+  padding-bottom: 0.7rem;
+`;
+const Paragraph = styled.p`
+  font-size: 1.2rem;
+`;
 
 export const Footer = () => {
   return (
@@ -21,32 +57,32 @@ export const Footer = () => {
           <Location>
             <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
             <div>
-              <p>San Jose.</p>
-              <p>California, United States.</p>
+              <Paragraph>San Jose.</Paragraph>
+              <Paragraph>California, United States.</Paragraph>
             </div>
           </Location>
           <Phone>
-            <h4>
+            <BoldWordsStyling>
               <FaPhone size={20} style={{ color: "#fff", marginRight: "2rem" }} />
               1-408-599-4331
-            </h4>
+            </BoldWordsStyling>
           </Phone>
           <Email>
-            <h4>
+            <BoldWordsStyling>
               <FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem" }} />
               marynghiem46@gmail.com
-            </h4>
+            </BoldWordsStyling>
           </Email>
         </Left>
         <Right>
-          <h4>About Me</h4>
-          <p>
-            I am currently a year into my coding journey which started with FreeCodeCamp and learning
-            HTML/CSS/Javascript and React.js. I then learned CS fundamentals by completing the Harvard CS 50 course. I
-            am now actively building web applications using React.js to create enjoyable websites for users. In my
-            newest project I am collaborating with multiple industry full stack engineers to build a Pictionary game
-            with a Pokemon theme!
-          </p>
+          <BoldWordsStyling>About Me</BoldWordsStyling>
+          <Paragraph>
+            My name is Mary Nghiem. I am currently a year into my coding journey which started with FreeCodeCamp and
+            learning HTML/CSS/Javascript and React.js. I then learned CS fundamentals by completing the Harvard CS 50
+            course. I am now actively building web applications using React.js to create enjoyable websites for users.
+            In my newest project I am collaborating with multiple industry full stack engineers to build a Pictionary
+            game with a Pokemon theme!
+          </Paragraph>
           <Social>
             <FaFacebook size={30} style={{ color: "#fff", marginRight: "1rem" }} />
             <FaTwitter size={30} style={{ color: "#fff", marginRight: "1rem" }} />

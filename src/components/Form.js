c71177491db2456170ed5d9ec4e3ea51d7c9b565
marynghiem <marynghiem46@gmail.com>
2022-07-22 14:13:55 -0700

    content of form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+const FormContainer = styled.div``;
+
 export const Form = () => {
-  return <div></div>;
+  return (
+    <FormContainer>
+      <form>
+        <label>Your Name</label>
+        <input type="text"></input>
+        <label>Email</label>
+        <input type="email"></input>
+        <label>Subject</label>
+        <input type="text"></input>
+        <label>Message</label>
+        <input rows="6" placeholder="Type your message here"></input>
+        <button className="btn">Submit</button>
+      </form>
+    </FormContainer>
+  );
 };

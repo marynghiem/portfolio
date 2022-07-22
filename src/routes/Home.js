daff5b98051659aa7eef0e59669d475c246810a9
marynghiem <marynghiem46@gmail.com>
2022-07-22 14:28:26 -0700

    Imported work component into home component and changed color of buttons

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import { HeroImg } from "../components/HeroImg";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import { Work } from "../components/Work";
 
 export const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroImg />
+      <Work />
       <Footer />
     </div>
   );

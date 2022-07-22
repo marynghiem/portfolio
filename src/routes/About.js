8ea5b54de793ee6e33104a8f876ed3b75364487c
marynghiem <marynghiem46@gmail.com>
2022-07-22 12:24:22 -0700

    set up about content component

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { HeroImg2 } from "../components/HeroImg2";
+import { AboutContent } from "../components/AboutContent";
 
 export const About = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="ABOUT." text="I'm a friendly Front-End Developer." />
+      <AboutContent />
       <Footer />
     </div>
   );

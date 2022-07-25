10b9cf6eb8c3cad256d06845e7e05268f3bdf1a1
marynghiem <marynghiem46@gmail.com>
2022-07-25 12:17:33 -0700

    added correct images to project data

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,19 +1,19 @@
 import React from "react";
 export const WorkCardData = [
   {
-    imgsrc: "coding.jpg",
+    imgsrc: "pokemongame.png",
     title: "Pokemon Pictionary Project",
     text: "A guessing game in which players attempt to identify words (pokemon) from pictures drawn by other players. In addition, the game includes many themes.",
     view: "https://draw-that-pokemon.web.app/",
   },
   {
-    imgsrc: "coding.jpg",
+    imgsrc: "foodwebsite.png",
     title: "Food Recipes and Reccomendation Website",
     text: "Designed and implemented a food website that utilizes the Spoonacular API to provide meal plans and nutrition information based on user calorie goals or a user’s available ingredients (React, Typescript, Spoonacular API, React Router). Added a Node.js server to proxy the 3rd party API request to avoid exposing the secret API key on the frontend",
     view: "https://marys-food-recipes.herokuapp.com/",
   },
   {
-    imgsrc: "coding.jpg",
+    imgsrc: "secretsanta.png",
     title: "Secret Santa",
     text: "Built a website with a shuffling algorithm that matches every participant with a Secret Santa and sends out emails to each person using a 3rd party API (React, Express, SendGrid API). Added frontend validation to prevent duplicate or empty user input, including regex validations for valid email addresses",
     view: "https://marys-food-recipes.herokuapp.com/",
